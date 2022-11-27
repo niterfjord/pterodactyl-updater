50b919df75449e0863a1085de5992439012fe8d9
Niterfjord <me@niterfjord.be>
2022-11-27 20:14:38 +0100

    Bugfixes

--- scripts/certificates.sh
+++ scripts/certificates.sh
@@ -2,13 +2,6 @@
 
 # COPYRIGHT @niterfjord @jobbedefeyter
 
-if ! [ -d "/var/log/niterfjord" ] ; then
-    mkdir /var/log/niterfjord
-fi
-if ! [ -d "/var/log/niterfjord/certbot-renewer" ] ; then
-    mkdir /var/log/niterfjord/certbot-renewer
-fi
-
 cd=`date +'%Y-%m-%d'`
 echo "===== CERTBOT RENEWER ${cd} ====="
 
